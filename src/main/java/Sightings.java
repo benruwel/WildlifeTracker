c56cc98754f61d17c74fdbb4725cbbeb6f45cb47
benruwel <benruwel21@gmail.com>
2020-07-10 19:54:32 +0300

    generated a POJO for the Sightings class

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,3 +1,24 @@
 public class Sightings {
 
+    private int id;
+    private String location;
+    private String rangerName;
+
+    public Sightings(int id, String location, String rangerName) {
+        this.id = id;
+        this.location = location;
+        this.rangerName = rangerName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
 }
