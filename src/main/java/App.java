992591b0322037c155bdb5fa114e80258eb24cfd
benruwel <benruwel21@gmail.com>
2020-07-12 17:20:01 +0300

    add the landing page and login prompt

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
+
+
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("username", request.session().attribute("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
